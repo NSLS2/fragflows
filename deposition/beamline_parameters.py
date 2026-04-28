05a3a106020f40615205a1920629beb8dd98ff1d
Kreitler, Dale <dkreitler@bnl.gov>
2026-04-28 17:06:49 -0400

    Only AMX params for now

---
+++ deposition/beamline_parameters.py
@@ -0,0 +1,20 @@
+
+
+BEAMLINE_PARAMETERS = {
+    "E-18-0104": {
+        "cif_update": {
+            "_diffrn_source.source": "SYNCHROTRON",
+            "_diffrn_source.type": "\"NSLS-II BEAMLINE 17-ID-1\"",
+            "_diffrn_source.pdbx_synchrotron_beamline": "17-ID-1",
+            "_diffrn_source.pdbx_synchrotron_site": "NSLS-II",
+            "_diffrn_detector.details": "KB",
+            "_diffrn_detector.detector": "PIXEL",
+            "_diffrn_detector.type": "\"DECTRIS EIGER X 9M\"",
+            "_diffrn_radiation.pdbx_scattering_type": "x-ray",
+            "_diffrn_radiation.pdbx_diffrn_protocol": "\"SINGLE WAVELENGTH\"",
+            "_diffrn_radiation.pdbx_monochromatic_or_laue_m_l": "m",
+            "_diffrn_radiation.monochromator": "\"DCM Si(111)\"",
+        },
+        "timezone": "US/Eastern",
+    },
+}
