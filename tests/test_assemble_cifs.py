d0f18271164439548fcfcf207ed3f839f2d2a4a3
Kreitler, Dale <dkreitler@bnl.gov>
2026-05-03 10:09:34 -0400

    update tests for changed state cif and sf cif

--- tests/test_assemble_cifs.py
+++ tests/test_assemble_cifs.py
@@ -5,10 +5,10 @@ from pathlib import Path
 group_dep_dir = "/nsls2/data/amx/proposals/2025-3/pass-319624/319624-cypd2-processing/test_group_deposition_20260424"
 
 def test_assemble_group_changed_state_cifs():
-    refinement_df = pd.read_csv("cypd.20260424.refinement.csv")
+    refinement_df = pd.read_csv("cypd.20260430.refinement.csv")
     event_df = pd.read_csv("cypd.event_table.20260422.csv")
     ligand_df = pd.read_csv("cypd.20251001.ligands_fixed.csv")
-    group_dep_dir = "../test_group_deposition_20260427"
+    group_dep_dir = "../test_group_deposition_20260430"
     assemble_group_changed_state_cifs(
         refinement_df,
         event_df,
@@ -17,7 +17,7 @@ def test_assemble_group_changed_state_cifs():
         ligand_df)
     # check that files are generated for each xtal_id in refinement_df
     for xtal_id in refinement_df['xtal_id']:
-        sf_cif_path = f"{group_dep_dir}/{xtal_id}-sf.mmcif"
+        sf_cif_path = f"{group_dep_dir}/{xtal_id}-sf.cif"
         assert Path(sf_cif_path).exists(), f"missing {sf_cif_path}"
-        cif_path = f"{group_dep_dir}/{xtal_id}.mmcif"
+        cif_path = f"{group_dep_dir}/{xtal_id}.cif"
         assert Path(cif_path).exists(), f"missing {cif_path}"
